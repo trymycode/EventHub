95ba5c2000e4d8066c13a4f7c586bf1100c50c82
trymycode <imitasingha1994@gmail.com>
2018-12-28 18:41:43 +0530

    basic server(localhost:3000)is ready

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+  res.send("Hello from server");
+})
+app.listen(PORT, function(){
+  console.log("server is running on localhost:- "+ PORT);
+})
