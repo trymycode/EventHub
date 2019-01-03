a3a3a3447f0f57099fa3ea6d455ab0c230dc57aa
trymycode <imitasingha1994@gmail.com>
2019-01-03 13:19:15 +0530

    completed login service(backend part)

--- eventApp/src/app/login/login.component.ts
+++ eventApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginUserData = {};
+  constructor( private _auth: AuthService ) { }
 
   ngOnInit() {
   }
-
+loginUser(){
+  this._auth.loginUser(this.loginUserData)
+  .subscribe(
+    res => console.log(res),
+    err => console.log(err)
+  )
+}
 }
