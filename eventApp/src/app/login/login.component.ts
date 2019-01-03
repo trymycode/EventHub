487372b3a52ed2383ea0e1fcdfdee82c5c68bfcf
trymycode <imitasingha1994@gmail.com>
2019-01-03 18:04:21 +0530

    storing JWT token in frontend

--- eventApp/src/app/login/login.component.ts
+++ eventApp/src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../auth.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,14 +8,18 @@ import { AuthService } from './../auth.service';
 })
 export class LoginComponent implements OnInit {
   loginUserData = {};
-  constructor( private _auth: AuthService ) { }
+  constructor( private _auth: AuthService, private _router: Router ) { }
 
   ngOnInit() {
   }
 loginUser(){
   this._auth.loginUser(this.loginUserData)
   .subscribe(
-    res => console.log(res),
+    res => {
+      console.log(res);
+      localStorage.setItem('token', res.token);
+      this._router.navigate(['/special']);
+    },
     err => console.log(err)
   )
 }
